e50fb810fe3a2319a3fc277add6422feb3ac5339
Daniel Oliveira <87251395+danielh-oliveira@users.noreply.github.com>
2025-07-30 16:34:58 -0300

    Inicio do 2 projeto
    Não consigo fazer direito

---
+++ gpt_projeto_2/script/obstaculo.gd
@@ -0,0 +1,12 @@
+extends StaticBody3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _physics_process(delta: float) -> void:
+	pass
